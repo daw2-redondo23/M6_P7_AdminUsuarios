98a98f2cb63cb2aaad77280b4916b45927c0781c
daw2-redondo23 <redondobarrosoruben@fpllefia.com>
2023-01-27 15:23:38 +0100

    inicial

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path';
+import path from "path";
+
+export default {
+    root: path.resolve(__dirname, "src"),
+    build: {
+        outDir: path.resolve(__dirname, "dist"),
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+               
+            },
+        },
+    },
+    base: './'
+};
